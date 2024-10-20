dbe4a70bd3c49f0c0b8138d28ddac141fd4944f7
Madiha Fatima <144526926+madihamalghani@users.noreply.github.com>
2024-10-20 11:52:35 +0500

    module

---
+++ module.py
@@ -0,0 +1,9 @@
+# pip3 install pyjokes => module
+import pyjokes
+print('Printing Jokes.......')
+joke=pyjokes.get_joke()
+print(joke)
+"""
+so this line will be ignored as a multi lined comment
+
+"""
